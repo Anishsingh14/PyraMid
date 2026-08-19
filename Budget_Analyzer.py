6f69ee97c16ee97c3da4e401e5678fc2e6656a86
Anish Singh <singhanish1404@gmail.com>
2026-08-19 23:32:07 +0530

    Pandas_Fundamentals
    Removed unnecessary comment line and added spacing for readability.

--- Budget_Analyzer.py
+++ Budget_Analyzer.py
@@ -22,7 +22,7 @@ def analyze_personal_csv():
     print("\nFile preview (First 3 rows):")
     print(df.head(3))
 
-    # DATA CLEANING & PREPROCESSING
+    
     df.columns = df.columns.str.strip().str.title()
     
     if "Amount" not in df.columns or "Category" not in df.columns:
