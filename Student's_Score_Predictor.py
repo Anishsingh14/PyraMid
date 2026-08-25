adbedf5fb46c6d545d6675c7109290ab1898d5f0
Anish Singh <singhanish1404@gmail.com>
2026-08-25 21:35:02 +0530

    Linear_Regression
    Added a comment to indicate where to load the dataset.

--- Student's_Score_Predictor.py
+++ Student's_Score_Predictor.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
-# 
+# Load Your dataset
 print("Loading data...")
 df = pd.read_csv('student_scores.csv')
 print(df.head())
